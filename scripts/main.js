5155db7e1f6f73a7190a9e472050fef280dc84d8
Yue Liu <yueliu96@uw.edu>
2020-08-26 11:09:39 -0700

    Add cookie for period selection

--- scripts/main.js
+++ scripts/main.js
@@ -50,7 +50,7 @@ let edgePaths = ["graph-1-120.json", "graph-1-2.json", "graph-3-9.json",
 ];
 
 // Default show the first option
-let index = period.selectedIndex;
+let index = getCookie("period", 0);
 
 for (let i = 0; i < profileItems.length; i++) {
     profileItems[i].style.display = "none";
@@ -68,7 +68,9 @@ d3.json('data/' + nodePath, function(nodes) {
 // Change displayed innerText if option of period change
 period.onchange = function() {
     profileItems[index].style.display = "none";
+
     index = this.selectedIndex
+    document.cookie = "period=" + index;
     profileItems[index].style.display = "inline";
 
     nodePath = nodePaths[index];
@@ -169,7 +171,7 @@ function plotSVG(nodes, edges, myclick, oriedges, minEdge, maxEdge) {
     // force.resume();
     force.start();
     // Add lines
-    console.log(minEdge, maxEdge);
+    // console.log(minEdge, maxEdge);
     let colorscale = d3.scale.linear().domain([minEdge, maxEdge]).range([0.2, 1]);
     let linescale = d3.scale.linear().domain([minEdge, maxEdge]).range([1, 10]);
     let svgEdges = svg.selectAll("line")
@@ -325,6 +327,21 @@ function plotSVG(nodes, edges, myclick, oriedges, minEdge, maxEdge) {
 
         return plotSVG(newNodes, newEdges, true, oriedges, minEdge, maxEdge);
     });
+}
+
+// return the value of cname in the cookie otherwise return cval
+function getCookie(cname, cval) {
+    let key = cname + "=";
+    let buffer = decodeURIComponent(document.cookie);
+    let cookies = buffer.split(";");
+    for (let i = 0; i < cookies.length; i++) {
+        let cooki = cookies[i];
+        let start = cooki.indexOf(key);
+        if (start != -1) {
+            return cooki.substring(start+key.length, cooki.length);
+        }
+    }
+    return cval
 }
 
 function validateXY(val, xy) {
